813db833962b0c9d7d03e13f34e3b1091dd8f333
Ben Garcia <ben@seebenprogram.com>
2018-02-22 13:22:24 -0500

    Add component 'api call result', toggleSrc method

---
+++ src/components/api-call-results/api-call-result.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- Bootstrap 'Card' -->
+  <div :class="cardClasses">
+
+    <!-- Gif still/animated image -->
+    <img 
+      @click="toggleSrc" 
+      :src="isAnimated ? animatedSrc : stillSrc"
+      :class="[imageClasses]"
+    >
+
+    <!-- Rating (G, PG, PG-13, etc.) -->
+    <p 
+      :class="ratingClasses"
+    >
+      Rating: {{ rating.toUpperCase() }}
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  // Each gif image to receive these props
+  props: [
+    "rating", 
+    "animatedSrc", 
+    "stillSrc"
+  ],
+  data() {
+    return {
+      // Bootstrap Card
+      cardClasses: [],
+      // Image
+      isAnimated: false,
+      imageClasses: [],
+      // Rating (G, PG, PG-13, etc.)
+      ratingClasses: []
+    }
+  },
+  methods: {
+    toggleSrc() {
+      this.isAnimated = !this.isAnimated;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
