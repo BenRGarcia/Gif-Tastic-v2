e4189b0120d1df004097506d41b1abdf3922ca02
Ben Garcia <ben@seebenprogram.com>
2018-02-22 13:22:59 -0500

    Build component for gif search results, receive props

---
+++ src/components/api-call-results/api-call-results.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+
+    <!-- Gif Search Result Instruction -->
+    <h2>
+      {{ gifText }}
+    </h2>
+
+    <!-- Gif Search Results -->
+    <result 
+      v-for="result in results"
+      :key="result.id"
+      :rating="result.rating" 
+      :stillSrc="result.images.original_still.url" 
+      :animatedSrc="result.images.original.url" 
+    />
+    
+  </div>
+</template>
+
+<script>
+import result from './api-call-result'
+
+export default {
+  props: [
+    "results"
+  ],
+  data() {
+    return {
+      gifText: "3) Click images to toggle animation"
+    }
+  },
+  components: {
+    result
+  },
+}
+</script>
+
+<style scoped>
+  
+</style>
