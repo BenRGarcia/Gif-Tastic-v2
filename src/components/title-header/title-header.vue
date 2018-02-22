ec73bacce53b032a25e6842150802a8e1e197000
Ben Garcia <ben@seebenprogram.com>
2018-02-22 10:18:38 -0500

    Build title-header component

---
+++ src/components/title-header/title-header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+
+    <!-- 'View Source Code' Link -->
+    <a 
+      :href="url" 
+      target="_blank"
+    >
+      {{ text }}
+    </a>
+  
+    <!-- Page Title -->
+    <h1 
+      :class="[titleClasses]"
+    >
+      {{ title }}
+    </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 'View Source Code' Link
+      url: "https://github.com/BenRGarcia/Gif-Tastic-v2",
+      text: "View Source Code",
+      // Page Title
+      title: "Gif-Tastic! v2",
+      titleClasses: ["display-3"]
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+
+}
+a:hover {
+  text-decoration: none;
+}
+</style>
