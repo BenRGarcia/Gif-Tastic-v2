06a3299372b3c22402e5600d66389833aa8c6512
Ben Garcia <ben@seebenprogram.com>
2018-02-22 11:05:47 -0500

    Build component logo-footer

---
+++ src/components/logo-footer/logo-footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <a 
+    :href="href" 
+    :class="footerClasses"
+    target="_blank"
+  >
+    <img 
+      :src="src"
+    >
+  </a>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // <a> atrributes
+      href: "https://giphy.com/",
+      footerClasses: ["fixed-bottom"],
+      // <img> attributes
+      src: require("../../assets/images/giphy-logo.png")
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  background: rgba(255,255,255,0.6);
+}
+</style>
