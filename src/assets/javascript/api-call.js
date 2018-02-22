1b6cf636bab0fd14240e17b7c7cd4a24a88722b1
Ben Garcia <ben@seebenprogram.com>
2018-02-22 09:56:19 -0500

    Build object that calls GIPHY API

---
+++ src/assets/javascript/api-call.js
@@ -0,0 +1,45 @@
+export default {
+  url: "https://api.giphy.com/v1/gifs/search",
+  apiKey: "yZgwRMNKMUe6DZ9WDUONVc4R1kCEEGFA",
+  limit: 6,
+  lastKeyword: "",
+  lastPromise: null,
+
+  search(keyword) {
+    // Screen out duplicate 'clicks' of same badge before calling API
+    if (keyword !== this.lastKeyword) {
+      this.lastKeyword = keyword;
+
+      // Replace spaces with '+' in keyword, assemble URL for api call
+      let query = this.sanitizeKeyword(keyword);
+      let url = this.makeQueryURL(query);
+
+      // Make API call
+      this.lastPromise = this.callAPI(url);
+    }
+    return this.lastPromise;
+  },
+
+  sanitizeKeyword(keyword) {
+    return keyword.replace(' ', '+');
+  },
+
+  makeQueryURL(sanitizedKeyword) {
+    let queryURL = this.url;
+    queryURL += '?' + $.param({
+      'q': sanitizedKeyword,
+      'limit': this.limit,
+      'api_key': this.apiKey
+    });
+    return queryURL;
+  },
+
+  callAPI(queryURL) {
+    return $.ajax({
+      url: queryURL,
+    })
+      .done( response => {
+        return response.data;
+      });
+  }
+}
