18f3d5b97b72cda473cf6b28ad0e6f134f613be6
Ben Garcia <ben@seebenprogram.com>
2018-02-22 13:21:09 -0500

    Add component 'results' for gif search results

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,16 @@
     <!-- Badges | New Badge Input & All Badges List -->
     <section class="row">
       <div class="col-12">
-        <badges/>
+        <badges 
+          @clickBadge="searchGifs"
+        />
       </div>
     </section>
 
     <!-- Gifs | Search Results: <img>'s, still/animated -->
     <section class="row">
       <div class="col-12">
-        
+        <results :results="gifResults"/>
       </div>
     </section>
 
@@ -35,13 +37,15 @@ import apiCall from './assets/javascript/api-call'
 import titleHeader from './components/title-header/title-header'
 import footerLogo from './components/logo-footer/logo-footer'
 import badges from './components/badges/badges'
+import results from './components/api-call-results/api-call-results'
 
 export default {
   name: 'App',
   components: {
     titleHeader,
     footerLogo,
-    badges
+    badges,
+    results
   },
   data() {
     return {
