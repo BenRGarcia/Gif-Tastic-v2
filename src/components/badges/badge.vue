977bdc71dabc849c48ca5004d42dbfc4c3ca3963
Ben Garcia <ben@seebenprogram.com>
2018-02-22 12:31:00 -0500

    Build badge component

---
+++ src/components/badges/badge.vue
@@ -0,0 +1,30 @@
+<template>
+  <span 
+    @click="clickBadge" 
+    :class="badgeClasses"
+  >
+    {{ badgeName }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: [
+    'badgeName'
+  ],
+  data() {
+    return {
+      badgeClasses: ["badge"]
+    }
+  },
+  methods: {
+    clickBadge(e) {
+      this.$emit("clickBadge", this.badgeName);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
